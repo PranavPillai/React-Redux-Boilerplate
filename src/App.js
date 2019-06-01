783b7f146b5536596e69f08432e7c6b1e9c3178a
Pranav Pillai <pillaip@seas.upenn.edu>
2019-05-31 23:33:46 -0400

    adds react router

--- src/App.js
+++ src/App.js
@@ -1,47 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Routes from './components/Routes';
 
-import { connect } from 'react-redux';
-import { getAccount } from './store/user/actions';
+const App = () => (
+  <div className="App">
+    <Routes />
+  </div>
+);
 
-class App extends React.Component {
-
-  componentDidMount() {
-    this.props.getAccount();
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-
-function mapStateToProps(state) {
-  return {
-    loggedIn: state.user.loggedIn
-  };
-}
-
-export default connect(
-  mapStateToProps, 
-  {
-    getAccount,
-  }
-)(App);
+export default App;
